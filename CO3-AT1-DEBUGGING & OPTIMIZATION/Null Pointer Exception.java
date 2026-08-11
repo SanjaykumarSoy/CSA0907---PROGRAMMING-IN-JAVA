749cf28ed40da2f84caae9e692ca5fa20a2d42ca
SanjaykumarSoy <157505017+SanjaykumarSoy@users.noreply.github.com>
2026-08-11 14:11:07 +0530

    Add null pointer exception handling in Q3 class

---
+++ CO3-AT1-DEBUGGING & OPTIMIZATION/Null Pointer Exception.java
@@ -0,0 +1,10 @@
+public class Q3 {
+    public static void main(String args[]) {
+        String name = null;
+        if (name != null) {
+            System.out.println("Length: " + name.length());
+        } else {
+            System.out.println("String is null");
+        }
+    }
+}
