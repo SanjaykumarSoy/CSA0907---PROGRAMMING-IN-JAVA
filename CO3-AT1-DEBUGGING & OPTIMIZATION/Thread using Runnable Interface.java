6c2eb3a577b01c1410edbe58276d901601a6706e
SanjaykumarSoy <157505017+SanjaykumarSoy@users.noreply.github.com>
2026-08-11 14:42:34 +0530

    Add Runnable implementation with thread execution

---
+++ CO3-AT1-DEBUGGING & OPTIMIZATION/Thread using Runnable Interface.java
@@ -0,0 +1,13 @@
+public class Q12 {
+    public static void main(String args[]) {
+        Demo d = new Demo();
+        Thread t = new Thread(d);
+        t.start();
+    }
+}
+class Demo implements Runnable {
+
+    public void run() {
+        System.out.println("Thread");
+    }
+}
